8c52d3b8ac8db4d3e60679bf74fbc0617624e0f8
Bronze14 <marcindadek@gmail.com>
2023-04-09 20:04:59 +0200

    update

--- src/Data.js
+++ src/Data.js
@@ -14,20 +14,20 @@ export default [
     },
     {
         id: 2,
-        link1: "https://adorable-elf-0f1615.netlify.app/",
-        link2: "https://github.com/Bronze14/Euflagle-Game",
-        title: "FLAGS GAME",
-        languages: "HTML CSS JS",
-        img: flagle1
-    },
-    {
-        id: 3,
         link1: "https://quizzl.netlify.app/",
         link2: "https://github.com/Bronze14/quiz",
 
         title: "QUIZ GAME",
         languages: "HTML CSS JS",
         img: quiz
+    },
+    {
+        id: 3,
+        link1: "https://adorable-elf-0f1615.netlify.app/",
+        link2: "https://github.com/Bronze14/Euflagle-Game",
+        title: "FLAGS GAME",
+        languages: "HTML CSS JS",
+        img: flagle1
     },
     {
         id: 4,
@@ -43,7 +43,7 @@ export default [
         link1: "https://main--van-shop.netlify.app/",
         link2: "https://github.com/Bronze14/Van-project",
         title: "VAN SHOP",
-        languages: "HTML CSS REACT",
+        languages: "HTML CSS REACT.JS",
         img: van2
 
     }
